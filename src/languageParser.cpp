a78f84c11be401a00c283a7aa5bd4a42d61f0d76
beans9926 <gulhanesarthak03@gmail.com>
2023-07-31 20:46:42 +0530

    added basic language/grammar parsing for player input

--- src/languageParser.cpp
+++ src/languageParser.cpp
@@ -6,19 +6,34 @@ std::vector<std::string> sepWordsVector; //stores separated words in a vector
 
 void WordSeparator(std::string action)
 {
-    std::stringstream ss(action);
-    std::stringstream cutString;
+    std::stringstream inputString(action);
+    std::stringstream choppedString;
     std::string word;
+    std::string temp;
      
     int i;
-    while(ss >> word){
-        //sepWordsVector.push_back(word);
-        
+
+    while(inputString >> word){
+        if(word == "and"){
+            sepWordsVector.push_back(temp);
+            choppedString.str("");
+            temp = "";
+        }
+        else if(inputString.eof() == true){
+            temp = word;
+            sepWordsVector.push_back(temp);
+        }
+        else{
+            choppedString << word;
+            temp = choppedString.str();   
+            choppedString << " ";
+            
+        }
     }
 
-    for(i = 0; i < sepWordsVector.size(); i++)
+    /*for(i = 0; i < sepWordsVector.size(); i++)
     {
         
-    } 
+    }*/ 
     
 }
